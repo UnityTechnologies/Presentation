bfe53524be31024e60d00e246d31919d5de06324
Valentin Simonov <v@lent.in>
2017-02-21 19:48:04 +0300

    Refactored a script.

--- Assets/Presentation/Unity/Template Slides/_SharedRecources/Scripts/TextGradientVertexColoring.cs
+++ Assets/Presentation/Unity/Template Slides/_SharedRecources/Scripts/TextGradientVertexColoring.cs
@@ -5,40 +5,37 @@ using System.Collections.Generic;
 public class TextGradientVertexColoring : BaseMeshEffect
 {
 
-    public Gradient brandingGradient;
+	public Gradient brandingGradient;
 
-    private List<UIVertex> m_VertexList = new List<UIVertex>();
+	private List<UIVertex> vertices = new List<UIVertex>();
 
-    public override void ModifyMesh(VertexHelper vh)
-    {
-         if (IsActive())
-         {
-              vh.GetUIVertexStream(m_VertexList);
-              int count = m_VertexList.Count;
-              float rightY = m_VertexList[0].position.x;
-              float leftY = m_VertexList[0].position.x;
+	public override void ModifyMesh(VertexHelper vh)
+	{
+		if (IsActive())
+		{
+			vh.GetUIVertexStream(vertices);
+			int count = vertices.Count;
+			float rightY = vertices[0].position.x;
+			float leftY = vertices[0].position.x;
 
-              for (int i = 1; i < count; i++)
-              {
-                   float y = m_VertexList[i].position.x;
-                   if (y > leftY)
-                        leftY = y;
-                   else if (y < rightY)
-                        rightY = y;
-              }
+			for (int i = 1; i < count; i++)
+			{
+				float y = vertices[i].position.x;
+				if (y > leftY) leftY = y;
+				else if (y < rightY) rightY = y;
+			}
 
-              float uiElementHeight = leftY - rightY;
+			float uiElementHeight = leftY - rightY;
 
-              UIVertex v = new UIVertex();
-              for (int i = 0; i < vh.currentVertCount; i++)
-              {
-                   vh.PopulateUIVertex(ref v, i);
-                   byte alpha = v.color.a;
-                   v.color = brandingGradient.Evaluate((v.position.x - rightY)/uiElementHeight);
-                   //v.color = Color32.Lerp(RightColor, LeftColor, (v.position.x - rightY) / uiElementHeight);
-                   v.color.a = alpha;
-                   vh.SetUIVertex(v, i);
-              }
-         }
-    }
+			UIVertex v = new UIVertex();
+			for (int i = 0; i < vh.currentVertCount; i++)
+			{
+				vh.PopulateUIVertex(ref v, i);
+				byte alpha = v.color.a;
+				v.color = brandingGradient.Evaluate((v.position.x - rightY) / uiElementHeight);
+				v.color.a = alpha;
+				vh.SetUIVertex(v, i);
+			}
+		}
+	}
 }
