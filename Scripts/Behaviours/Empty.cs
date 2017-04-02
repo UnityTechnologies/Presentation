e2b08a160969569bac34fd0ba305f5eaaaa1a265
Valentin Simonov <v@lent.in>
2017-04-03 02:04:20 +0300

    Empty scene takes current slide deck background color by default eliminating unnecessary bg color blinking.

---
+++ Scripts/Behaviours/Empty.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.Presentation 
+{
+
+	[ExecuteInEditMode]
+	public class Empty : MonoBehaviour
+	{
+
+		private void Awake()
+		{
+			var deck = Engine.Instance.SlideDeck;
+			if (deck == null) return;
+			GetComponent<Camera>().backgroundColor = deck.BackgroundColor;
+		}
+
+	}
+}
